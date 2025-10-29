12a5e57138ad42f3058987381091188b2c6b116f
jdguerrero28 <jdiegogueal@gmail.com>
2025-10-29 12:04:47 -0500

    Salto de Linea

--- Ejemplo01/src/ejemplo01/Ejemplo04.java
+++ Ejemplo01/src/ejemplo01/Ejemplo04.java
@@ -18,5 +18,16 @@ public class Ejemplo04 {
         System.out.print(apellidoEstudiante + " ");
         System.out.print(nacimiento + " ");
         System.out.print(nombreEstudiante + " ");
+        // La diferencia entre "print" y "println" es que el "println" da un salto de linea mientras que el "print" no lo hace
+        System.out.println("-----------------");
+        System.out.println(nombreEstudiante + " ");
+        System.out.println(apellidoEstudiante + " ");
+        System.out.println(nacimiento + " ");
+        System.out.println(nombreEstudiante + " ");
+    
+    
+    
+    
+    
     }
 }
